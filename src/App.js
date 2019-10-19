6d647abda1987b4a65d4bc7be7a46d19690d2785
Roblouisck <robert@eddiebernard.com>
2019-10-18 19:25:26 -0700

    Add Header Skeleton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import Header from './Header'
+import ActivityFeed from './ActivityFeed'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-
-
-        <h1>Hello World</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi necessitatibus sit dolore aperiam beatae consectetur doloremque at excepturi, quisquam inventore! Sunt illo natus recusandae. Ex nihil, et nemo sapiente culpa!</p>
+        <ActivityFeed />
       </div>
     )
   }
--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,31 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div>
-        <input 
-          className="searchBox"
-          type="search"
-          placeholder="Search"
-        />
+      <div className="header">
+      
+        <div className="hamburgerMenu">
+          <div className="hamburgerLine"></div>
+          <div className="hamburgerLine"></div>
+          <div className="hamburgerLine"></div>
+        </div>
+
+        <div className="searchBar">
+
+          <input className="searchBox"
+            type="search"
+            placeholder="Search"
+          />
+
+          <div className="magnifyingGlass">
+            <div className="magnifyBackground">
+              <span className="magnifyIcon">
+                <svg className="p" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="signInBackground">
+        </div>
       </div>
     )
   }
