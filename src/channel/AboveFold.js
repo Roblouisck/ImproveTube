f43b5659d026d512b535eae0a892c52e41e50a45
Roblouisck <robert@eddiebernard.com>
2019-11-20 03:42:37 -0800

    Match Channel Page to Design
    Exact margins, paddings, widths, etc. I also redid some media queries to make them more visually appealing.

--- src/channel/AboveFold.js
+++ src/channel/AboveFold.js
@@ -6,35 +6,39 @@ class AboveFold extends React.Component {
   }
 
   componentDidMount = () => {
-    function windowSize1000(mediaQuery904) {
-      const subscribersSection = document.querySelector('.channel--aboveFold-subscribers-box-wrapper')
-      const channelAvatar = document.querySelector('.channel--aboveFold-avatar-content-wrapper')
-      const descriptionBox = document.querySelector('.channel--aboveFold-description-box')
+    function windowSize940(mediaQuery940) {
+      const followButton = document.querySelector('.channel--grid-nav-follow')
+      const channelAvatar = document.querySelector('.channel--aboveFold-avatar')
+      const gridNavSubscriptionText = document.querySelector('.channel--grid-nav-subscriptions')
     
-      if (mediaQuery904.matches) { 
-        subscribersSection.parentNode.insertBefore(subscribersSection, channelAvatar.nextSibling)
+      if (mediaQuery940.matches) { 
+        channelAvatar.parentNode.insertBefore(followButton, channelAvatar.nextSibling)
       } else {
-        descriptionBox.parentNode.insertBefore(subscribersSection, descriptionBox.nextSibling)
+        gridNavSubscriptionText.parentNode.insertBefore(followButton, gridNavSubscriptionText.nextSibling)
       }
     }
 
-    function windowSize580(mediaQuery580) {
+    function windowSize580(mediaQuery580, mediaQueryBetween) {
       const followButton = document.querySelector('.channel--grid-nav-follow')
       const avatarFollowerText = document.querySelector('.channel--aboveFold-follower-text')
-      const gridNavSubscriptions = document.querySelector('.channel--grid-nav-subscriptions')
+      const channelAvatar = document.querySelector('.channel--aboveFold-avatar')
+      const gridNavSubscriptionText = document.querySelector('.channel--grid-nav-subscriptions')
     
       if (mediaQuery580.matches) { 
         avatarFollowerText.parentNode.insertBefore(followButton, avatarFollowerText.nextSibling)
-      } else {
-        gridNavSubscriptions.parentNode.insertBefore(followButton, gridNavSubscriptions.nextSibling)
+      } else if (mediaQueryBetween) {
+        gridNavSubscriptionText.parentNode.insertBefore(followButton, gridNavSubscriptionText.nextSibling)
+      } else if (window.matchMedia("(max-width: 940px)").matches && (window.matchMedia("(min-width: 580px)").matches)) {
+        channelAvatar.parentNode.insertBefore(followButton, channelAvatar.nextSibling)
       }
     }
 
-    const mediaQuery904 = window.matchMedia("(max-width: 904px)")
+    const mediaQuery940 = window.matchMedia("(max-width: 940px)")
+    const mediaQueryBetween = window.matchMedia("(max-width: 99999px) and (min-width: 940px)")
     const mediaQuery580 = window.matchMedia("(max-width: 580px)")
-    mediaQuery904.addListener(windowSize1000)
-    mediaQuery580.addListener(windowSize580)
-    windowSize1000(mediaQuery904)
+    mediaQuery940.addListener(windowSize940)
+    mediaQuery580.addListener(windowSize580, mediaQueryBetween)
+    windowSize940(mediaQuery940)
     windowSize580(mediaQuery580)
   }
 
@@ -54,7 +58,7 @@ class AboveFold extends React.Component {
           <p>Dear friends,</p>
           <br/>
           <p>
-            My name is Lyz and I`m a mentor for others who have dreams to travel, live abroad, and descriptionBoxtain a better lifestyle whilst living abroad. I have lived in Japan for a total of five years and learned quite a lot -not just about the country but myself as well. Not only do I want to inform others about life abroad but my descriptionBox goal is to get you organized, stay mindful, and descriptionBoxtain a healthy and exciting lifestyle whether you plan on living in another country or not. This site is not only for adventure lovers but for inspiration seekers. All of what I have gained and endured will allow me to mold you into an adventurer yourself!
+            My name is Lyz and I`m a mentor for others who have dreams to travel, live abroad, and gridNavSubscriptionTexttain a better lifestyle whilst living abroad. I have lived in Japan for a total of five years and learned quite a lot -not just about the country but myself as well. Not only do I want to inform others about life abroad but my gridNavSubscriptionText goal is to get you organized, stay mindful, and gridNavSubscriptionTexttain a healthy and exciting lifestyle whether you plan on living in another country or not. This site is not only for adventure lovers but for inspiration seekers. All of what I have gained and endured will allow me to mold you into an adventurer yourself!
           </p>
           <br/>
           <p>love,</p>
