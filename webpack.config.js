0ef58acc533d0282d1879d90b412cd183288d12c
Roblouisck <robert@eddiebernard.com>
2019-12-29 06:33:24 -0800

    Remove Webpack and Babel Config From gitignore

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: 'dist',
+        historyApiFallback: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/public/index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new Dotenv()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    { loader: 'css-loader', options: { sourceMap: true} },
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ],
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
