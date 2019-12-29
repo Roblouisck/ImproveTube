e12c1dfd498e13f7be148037034bdd07990e4580
Roblouisck <robert@eddiebernard.com>
2019-12-29 13:45:39 -0800

    Setup Mobile Controls

--- src/components/FooterMobile.js
+++ src/components/FooterMobile.js
@@ -6,26 +6,33 @@ import {
   subscriptions } from './svgs'
 
 const FooterMobile = () => {
+  const handleActivityFeedButton = () => {
+    document.querySelector('.home--grid-background').classList.toggle('hide')
+    document.querySelector('.activityFeedContainer').classList.toggle('show')
+    document.querySelector('.channel--mobile-footer-button-activityFeed').classList.toggle('button-background-red')
+  }
+
   const [p, setResource] = useState('channel--mobile')
 
     return (
       <footer className={`${p}-footer`}>
-        <div>
-          {home()}
-          <p className={`${p}-footer-text`}>Home</p>
+        <div className={`${p}-footer-button-home`}>
+          <a href="/">{home()}
+            <p className={`${p}-footer-text`}>Home</p>
+          </a>
         </div>
 
-        <div>
+        <div className={`${p}-footer-button-activityFeed`} onClick={() => handleActivityFeedButton()}>
           {activityFeed()}
           <p className={`${p}-footer-text`}>Activity Feed</p>
         </div>
 
-        <div>
+        <div className={`${p}-footer-button-following`}>
           {following()}
           <p className={`${p}-footer-text`}>Following</p>
         </div>
 
-        <div>
+        <div className={`${p}-footer-button-subscriptions`}>
           {subscriptions()}
           <p className={`${p}-footer-text`}>Subscriptions</p>
         </div>
