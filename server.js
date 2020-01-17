7bdda8bee06db61acb98f051ab73445b2e2581cd
Roblouisck <robert@eddiebernard.com>
2020-01-16 16:24:15 -0800

    test

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
-app.use(express.static(__dirname))
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.js'))
+  res.sendFile(path.resolve(__dirname, './src/public/index.html'))
 })
 
 app.listen(port)
