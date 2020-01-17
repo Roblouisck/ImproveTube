6f1e5680738df7a6e6a54e1d7561a2eb7fa08762
Roblouisck <robert@eddiebernard.com>
2020-01-16 16:12:42 -0800

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static(__dirname))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './src/public/index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.js'))
 })
 
 app.listen(port)
